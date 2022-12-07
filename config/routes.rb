5e35ae3db58d6626bee223986bbcdf79e7ac6a6e
shamilsdq <shamilsdq@live.com>
2022-12-07 12:39:32 +0530

    Scoped devise routes under api path, and defaulted to json response

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope :api, defaults: { format: :json } do
+    devise_for :users
+  end
 end
