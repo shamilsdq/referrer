8bec3fc8f1bd7e62110ac102fbbd4eaa6029c307
shamilsdq <shamilsdq@live.com>
2022-12-07 13:14:00 +0530

    Added method to generate JWT token in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
+
+  def generate_auth_token
+    JWT.encode(
+      { id: id, exp: 10.days.from_now.to_i },
+      Rails.application.secrets.secret_key_base)
+  end
 end
