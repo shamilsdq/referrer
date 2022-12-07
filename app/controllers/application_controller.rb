2dad7a45ba7cb4ce38e68cf1953f5e59d12d0d0a
shamilsdq <shamilsdq@live.com>
2022-12-07 14:15:39 +0530

    Setup authentication methods in a concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  respond_to :json
+
+  include Authenticatable
 end
