40366fcd54db3ac1cc15c72c11ff6d162b6c4865
shamilsdq <shamilsdq@live.com>
2022-12-08 14:08:24 +0530

    Configured react with an App component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,3 @@
-import "stylesheets/application";
-
 // Support component names relative to this directory:
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>REACT HOME</div>;
+
+export default App;
